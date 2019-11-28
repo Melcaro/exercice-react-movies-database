38f33d7bd8802007d8df251d4cc0f9fd2d9442a6
melanie caro <melanie.caro@stackerine.com>
2019-11-28 10:07:27 +0100

    start a movies database app

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>MOVIES DATABASE</h1>
+        <div className="movieContainer">
+          
+
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
